94660f0f48549412579a7a945b27137e5d98198e
NaoP <naop18@yahoo.fr>
2023-10-09 22:56:24 +0200

    création de login.html, login.js

--- Backend/script.js
+++ Backend/script.js
@@ -1,6 +1,22 @@
+let gallery = document.getElementsByName('gallery')
+let figure = document.createElement("figure")
+let img = document.createElement("img")
+let figcaption = document.createElement("figcaption")
+
+
+
+
 fetch("http://localhost:5678/api/works")
     .then(reponse => reponse.json())
-    .then(data => img.src=data.url)
-    /* Au lieu du imagesource creer un create element et un happen 
-    child qui va creer a chaque boucle une figure/img/figcaption comme dans le html, pas besoin d'id pcq y en a pas
-    je dois pas copier coller le code du HTML */
+    .then(data => {
+        gallery.appendChild(figure)
+        figure.appendChild(img)
+        figure.appendChild(figcaption)
+        img = `< img src = "${data.imageURL[1]}"/>`
+    });
+
+
+
+/* Au lieu du imagesource creer un create element et un appen
+child qui va creer a chaque boucle une figure/img/figcaption comme dans le html, pas besoin d'id pcq y en a pas
+je dois pas copier coller le code du HTML */
