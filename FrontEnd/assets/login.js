d223356bf4330f27d7a6a3d27d01b40c87f1014e
NaoP <naop18@yahoo.fr>
2023-10-15 13:41:37 +0200

    ajout des images dans l'acceuil et de la page login

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault(); // Empêche la soumission par défaut
+
+        if (emailInput.value === '' || passwordInput.value === '') { // Vérifie que les champs sont remplis
+            alert('Veuillez remplir tous les champs.');
+            return;
+        }
+
+        try {
+            const response = await fetch('http://localhost:5678/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: emailInput.value,
+                    password: passwordInput.value,
+                })
+            });
+
+            if (response.ok) {
+                console.log('Connexion réussie');
+                window.location = 'index.html'; // Redirige l'utilisateur vers la page d'accueil
+                alert('Connexion réussie !');
+            } else {
+                console.error('Échec de la connexion');
+                let error = document.createElement('p');
+                error.textContent = 'Identifiant ou mot de passe incorrect';
+                /* error.style = nom classe css */
+                form.appendChild(error);
+            }
+        } catch (error) {
+            console.error('Erreur inattendue:', error);
+            alert('Une erreur est survenue lors de la connexion.');
+        }
+    });
+});
---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,33 @@
+const gallery = document.getElementsByClassName('gallery')
+
+
+fetch("http://localhost:5678/api/works")
+    .then(reponse => reponse.json())
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            let figure = document.createElement("figure")
+            let img = document.createElement("img")
+            let figcaption = document.createElement("figcaption")
+            console.log(gallery[0])
+            img.src = data[i].imageUrl
+            figcaption.textContent = data[i].title
+            gallery[0].appendChild(figure)
+            figure.appendChild(img)
+            figure.appendChild(figcaption)
+        }
+    });
+
+
+
+
+
+
+
+
+
+/* Au lieu du imagesource creer un create element et un appen
+child qui va creer a chaque boucle une figure/img/figcaption comme dans le html, pas besoin d'id pcq y en a pas
+je dois pas copier coller le code du HTML 
+Attribuer une classe CSS en JS pour le mdp oublié
+Mettre les filtres en format texte puis JS simple
+*/
