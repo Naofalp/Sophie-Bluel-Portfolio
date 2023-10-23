507bc8877534240c01c8ab5075748333162e2b2c
NaoP <naop18@yahoo.fr>
2023-10-23 16:35:08 +0200

    Vérifie si l'utilisateur est déjà connecté

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form');
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
@@ -10,6 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('Veuillez remplir tous les champs.');
             return;
         }
+
+        // Vérifie si l'utilisateur est déjà connecté
+        if (window.sessionStorage.getItem('token') !== null) {
+            alert('Vous êtes déjà connecté');
+            return;
+        }
+
 
         try {
             const response = await fetch('http://localhost:5678/api/users/login', {
@@ -32,12 +39,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 let error = document.createElement('p');
                 error.textContent = 'Identifiant ou mot de passe incorrect';
                 /* error.style = nom classe css */
-                form.parentNode.insertBefore(error, form);            }
+                form.parentNode.insertBefore(error, form);
+            }
 
-                
+            const result = await response.json();
+            const token = result.token;
+            window.sessionStorage.setItem('token', token); // Comment savoir si le token est stoken.
+
+
         } catch (error) {
             console.error('Erreur inattendue:', error);
             alert('Une erreur est survenue lors de la connexion.');
         }
     });
 });
+
+
+/* Compte de test pour Sophie Bluel
+
+sophie.bluel@test.tld
+
+S0phie 
+*/
