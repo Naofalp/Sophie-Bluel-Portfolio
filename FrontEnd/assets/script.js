4f610a1e36f20d214cc6d24c23c95865a05c817d
NaoP <naop18@yahoo.fr>
2023-11-03 20:11:02 +0100

    creation fonction main + avancée des l'affichage filtre

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -6,8 +6,9 @@ const portfoliosection = document.querySelector('.portfolio');
 // Affichage admin connecté
 async function Loged() {
     if (window.sessionStorage.getItem('token') === null) {
-        categoriesFilters();
+        main();
     } else {
+        works();
         topBarBlack();
         Logout();
         //Ajouter modale qui apparait
@@ -18,16 +19,15 @@ async function Loged() {
 function Logout() {
     const logoutButton = document.getElementById('login');
     logoutButton.textContent = 'logout';
-    logoutButton.setAttribute ('href','index.html') /* Rafraichie la page au click*/
+    /* logoutButton.setAttribute('href', 'index.html') // On reste sur la page d'accueil */
     logoutButton.addEventListener('click', () => {
-        window.sessionStorage.removeItem('token');
+        window.sessionStorage.removeItem('token'); //reprendre la supp du token
         console.log('Vous êtes déconnecté');
         alert('Vous êtes déconnecté.');
-        //Comment rester sur l'ecran d'acceuil ?
     });
 }
 
-// TopBar Mode Edition
+// TopBarBlack (Mode Edition)
 function topBarBlack() {
     const div = document.createElement("div");
     div.setAttribute('class', 'TopBar');
@@ -44,17 +44,18 @@ function topBarBlack() {
 }
 
 
-
+//Affichage des travaux
 async function works() {
 
     fetch("http://localhost:5678/api/works")
         .then(reponse => reponse.json())
         .then(data => {
+            console.log(data)
             for (let i = 0; i < data.length; i++) {
                 let figure = document.createElement("figure")
                 let img = document.createElement("img")
                 let figcaption = document.createElement("figcaption")
-                console.log(gallery[0])
+                // console.log(gallery[0])
                 img.src = data[i].imageUrl
                 figcaption.textContent = data[i].title
                 gallery[0].appendChild(figure)
@@ -65,12 +66,17 @@ async function works() {
     ;
 }
 
+// Lien modifier modal a cote de H2 A FAIRE 
+function modifyButton() {
+    const MyProjects = document.getElementById('MyProjects')
+}
 
+//Filtres par catégories
 async function categoriesFilters() {
     fetch("http://localhost:5678/api/categories")
         .then(reponse => reponse.json())
         .then(categories => {
-            console.log(categories)
+           // console.log(categories)
 
             const filters = document.createElement("div");
             filters.classList.add('categories');
@@ -78,39 +84,47 @@ async function categoriesFilters() {
             const ButtonAll = document.createElement("button");
             ButtonAll.textContent = 'Tous';
             ButtonAll.classList.add('button', 'tous');
+            ButtonAll.addEventListener('click', () => filterImages("Tous"));
             filters.appendChild(ButtonAll)
 
             for (let category of categories) {
                 const button = document.createElement('button');
                 button.setAttribute('class', 'button');
                 button.textContent = `${category.name}`;
+                button.addEventListener('click', () => filterImages(category.name));
                 filters.appendChild(button)
             }
 
             portfoliosection.insertBefore(filters, gallery[0]);
-
-
-            function filterImages(categoryName) {
-                const allImages = gallery.querySelectorAll('figure');
-                allImages.forEach(image => {
-                    const imageCategory = image.dataset.category;
-                    if (categoryName === "Tous" || imageCategory === categoryName) {
-                        image.style.display = "block";
-                    } else {
-                        image.style.display = "none";
-                    }
-                });
-            }
         });
     ;
 }
 
+// Matcher la cate works et categories. Refaire un appel API.
 
-works();
+function filterImages(categoryName) {
+    // console.log(gallery);
+    const allImages = gallery[0].querySelectorAll("figure");
+    allImages.forEach(image => {
+        const imageCategory = image.dataset.category;
+        // console.log(image)
+        if (categoryName === "Tous" || imageCategory === categoryName) {
+            image.style.display = "block";
+        } else {
+            image.style.display = "none";
+        }
+    });
+}
+
+async function main() {
+    await works();
+    categoriesFilters();
+}
+
+//main();
+// works();
 //categoriesFilters();
 Loged();
 
-/* 
-Attribuer une classe CSS en JS pour le mdp oublié
-Mettre les filtres en format texte puis JS simple
-*/
+// Ca marche mais ca passe toute les images en display:none, 
+//verifier que c'est bien relier a sa category name.
